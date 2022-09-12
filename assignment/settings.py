08f36e62f2636a7d5054ef5e5f6694b40bcad1de
Aom Siriwat <6310680233@eng.tu>
2022-09-12 14:40:26 +0700

    add assignment app config

--- assignment/settings.py
+++ assignment/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'assignment.apps.AssignmentConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
